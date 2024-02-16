a98390b61cc884682617fe4b4c787903b3845a45
Renan Okanha <okanharenan@gmail.com>
2024-02-16 20:40:21 -0300

    Criação de view, template, e url

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Created view home 
+def home(request):
+    '''Criação da view home'''
+    return render(request,'recipes/base.html')
