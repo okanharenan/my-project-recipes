e25aabfa36fae8f0eab78ea39dd500c4a26a8a5e
Renan Okanha <okanharenan@gmail.com>
2024-06-30 22:37:26 -0300

    Adicionando coverage (cobertura de teste)

--- recipes/tests/test_recipes_models_recipe.py
+++ recipes/tests/test_recipes_models_recipe.py
@@ -2,7 +2,7 @@
 from django.core.exceptions import ValidationError
 from parameterized import parameterized
 
-from .test_recipe_base import RecipeTestBase
+from .test_recipe_base import RecipeTestBase , RecipeModel 
 
 class RecipeModelTest(RecipeTestBase):
     def setUp(self) -> None:
@@ -19,3 +19,29 @@ class RecipeModelTest(RecipeTestBase):
         setattr(self.recipe, field, 'A' * (max_length + 1))
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
+
+    def make_recipe_no_default(self):
+        recipe = RecipeModel(
+            category=self.make_category(name = "Test is Default is False"),
+            author=self.make_author(username="username is Renan"),
+            title='teste',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_step='Recipe Preparation Steps',
+            preparation_step_is_html=False,
+            is_published=True,
+    )
+        recipe.full_clean()
+        recipe.save()
+        return recipe
+    
+    def test_recipe_is_published_is_false_by_default(self):
+        recipe = self.make_recipe_no_default()
+        self.assertFalse(recipe.is_published,
+                         msg="recipe is_published is not false")
+        
+    
