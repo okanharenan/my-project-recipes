b98b90c1542294b38a68471488d4814ee54b7894
Renan Okanha <okanharenan@gmail.com>
2024-07-15 01:36:00 -0300

    ajustando  o estilo do formulário.

--- authors/forms.py
+++ authors/forms.py
@@ -7,3 +7,32 @@ class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ("first_name", "last_name", "username", "email", "password")
+        #exclude = ["last_name"] exemplo para exclusão de campo
+
+        labels = {
+            'first_name' : 'First name',
+            'last_name' : 'Last name',
+            'username' : 'UserName',
+            'email' : 'E-mail',
+            'password' : 'Password',
+        }
+        help_texts = {
+            'email' : 'the e-mail must be valid.'
+        }
+        
+        error_messages = {
+            'username' : {
+                'requerid': 'this field must not be empty'
+            }
+        }
+
+        widgets = {
+            'first_name' : forms.TextInput(attrs={
+                'placeholder' : 'Type yuor first name here',
+                'class' : 'input text-input'
+            }),
+
+            'password' : forms.PasswordInput(attrs={
+                'placeholder' : 'Type yuor Password here'
+            })
+        }
