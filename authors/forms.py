a62a8e9f8e78dca56ad29f1b96cc644e0a90a466
Renan Okanha <okanharenan@gmail.com>
2024-07-17 01:58:26 -0300

    sobrescrevendo  campos do forms que necessita ajuste e criando validação personalizadas

--- authors/forms.py
+++ authors/forms.py
@@ -1,8 +1,54 @@
 from django import forms 
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 
+def add_attr(field, attr_name, attr_new_val):
+    existing_attr = field.widget.attrs.get('attr_name', '')
+    field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip() 
+
+def add_placeholder(field, placeholder_val):
+    add_attr(field, 'placeholder', placeholder_val)
     
 class RegisterForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Your Username')
+        add_placeholder(self.fields['email'], 'Youre-mail@email.com')
+        add_placeholder(self.fields['first_name'], 'Nome')
+        add_placeholder(self.fields['last_name'], 'Sobrenome')
+        add_attr(self.fields['username'], 'css', 'a-css-class')
+
+
+    password = forms. CharField(
+    
+        widget= forms.PasswordInput(attrs={
+            'placeholder': 'Type yuor Password here',
+            'class' : 'input text-input'
+        }),
+        error_messages={
+            'requerid': 'Password must not be empty'
+        },
+
+        help_text=(
+            'Password must have at least one uppercase letter'
+
+            'one lowercase letter  and one number. The length should be '
+
+            'at lest 8 characters.'
+    )
+        
+    )
+
+    password2 = forms.CharField(
+        
+        widget= forms.PasswordInput(attrs={
+            'placeholder': 'Repeat yuor password'
+        }),
+        label= 'Password 2'
+    )
+
+    
 
     class Meta:
         model = User
@@ -26,13 +72,22 @@ class RegisterForm(forms.ModelForm):
             }
         }
 
-        widgets = {
-            'first_name' : forms.TextInput(attrs={
-                'placeholder' : 'Type yuor first name here',
-                'class' : 'input text-input'
-            }),
+    
+    def clean(self):
+        cleaned_data = super().clean()
 
-            'password' : forms.PasswordInput(attrs={
-                'placeholder' : 'Type yuor Password here'
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+
+        if password != password2:
+            password_confirmation_error = ValidationError(
+                'Password and password2 must be equal',
+                code='invalid'
+            )
+            raise ValidationError({
+                'password': password_confirmation_error,
+                'password2': [
+                    password_confirmation_error,
+                ],
             })
-        }
+
--- authors/views.py
+++ authors/views.py
@@ -13,10 +13,11 @@ def register_view(request):
     return render(request, 'authors/pages/register_view.html', context)
 
 def register_create(request):
-    if  not request.Post:
+    if  not request.POST:
         raise Http404()
+    
     POST = request.POST
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
-    redirect('authors:register')
+    return redirect('authors:register')
     
